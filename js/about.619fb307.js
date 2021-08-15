00a188b3b90c166ce334a6659559de0eecae15ba
ilya-belik <60644182+ilya-belik@users.noreply.github.com>
2021-08-15 16:14:24 +0000

    Build: (9e0e10d) заменены лицензия на MIT

---
+++ js/about.619fb307.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{d504:function(n,e,d){"use strict";function o(n,e,d,o,r,t){return" Index "}d.r(e);var r={name:"Index"};r.render=o;e["default"]=r}}]);
+//# sourceMappingURL=about.619fb307.js.map
